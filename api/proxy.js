cd97f2538902cbafa97df0ea369b27420517f222
FreeOJDEV <FojPro@outlook.com>
2024-10-12 20:50:47 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "github.com";//your website url
+  let target = "hydro.ac";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
